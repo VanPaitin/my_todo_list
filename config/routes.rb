91c12666b718ca21d776f04c7ddadfed4d523ccd
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-26 19:10:45 +0100

    feat: use method override in rack app

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 TodoList.routes.draw do
   get "/todos", to: "todolist#index"
-  get "/todo/:id", to: "todolist#show"
   get "/todo/new", to: "todolist#new"
+  get "/todo/:id", to: "todolist#show"
   get "/todo/:id/edit", to: "todolist#edit"
-  post "/todo/:id", to: "todolist#create"
+  post "/todos", to: "todolist#create"
   put "/todo/:id", to: "todolist#update"
   patch "/todo/:id", to: "todolist#update"
   delete "/todo/:id", to: "todolist#destroy"
   get "/pages/about", to: "pages#about"
   get "/pages/tell_me", to: "pages#tell_me"
+  root "todolist#index"
 end
